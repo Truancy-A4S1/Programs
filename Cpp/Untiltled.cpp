9e53944ac216fe1602a01138fd0bf74602f3801b
Ryan <ryanjamilosa.1@gmail.com>
2023-02-18 20:43:19 +0800

    Initital Commit

---
+++ Cpp/Untiltled.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int input = 0;
+    cout << "Enter input: "; cin >> input;
+    cout << "Input is " << input << endl;
+    return 0;    
+}
---
+++ Java/Untitled/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+        System.out.println("Hilu Wurld!");
+    }
+}
