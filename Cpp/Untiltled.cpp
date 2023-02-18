4f05a0a88b3cc6a06ebdee418730cc82cfb838e3
Ryan <ryanjamilosa.1@gmail.com>
2023-02-18 20:46:36 +0800

    Tried to edit a file

--- Cpp/Untiltled.cpp
+++ Cpp/Untiltled.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
     int input = 0;
     cout << "Enter input: "; cin >> input;
-    cout << "Input is " << input << endl;
+    cout << "Input isers " << input << endl;
     return 0;    
 }
