84a5790397dd577616ae4565cd4a27dcffb45813
Ryan <ryanjamilosa.1@gmail.com>
2023-02-20 09:02:02 +0800

    Test

--- Cpp/Untiltled.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Hello" << endl;
-    cout << "That's VS Code for you!" << endl;
-    cout << "Bye!" << endl
-    return 0;    
-}
