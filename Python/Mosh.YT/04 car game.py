4afa72b185437806eada550afc1ec55820d04bb7
Ryan <ryanjamilosa.1@gmail.com>
2023-02-23 17:51:14 +0800

    Simple game for fourth day

---
+++ Python/Mosh.YT/04 car game.py
@@ -0,0 +1,32 @@
+want_to_quit = False
+command = "Initial"
+prev_state = "Initial"
+
+while want_to_quit == False:
+    command = input(" >")
+
+    if command.lower() == "help":
+        print("\tstart - to start the car")
+        print("\tstop - to stop the car")
+        print("\tquit - to exit")
+
+    elif command.lower() == "start":
+        if command == prev_state:
+            print("Car already started.")
+        else:
+            print("Car started... Ready to go!")
+
+    elif command.lower() == "stop":
+        if command == prev_state:
+            print("Car already stopped.")
+        else:
+            print("Car stopped.")
+
+    elif command.lower() == "quit":
+        print("Bye!")
+        want_to_quit = True
+
+    else:
+        print("I don't understand that...")
+
+    prev_state = command
